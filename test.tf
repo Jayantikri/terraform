b3320229e476cd94ebf98ecfd0dd6df3b6dd6437
Jayantikri <155369897+Jayantikri@users.noreply.github.com>
2024-04-10 17:47:19 +0530

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    access_key = "**********"
+    secret_key = "*******"
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "ec1" {
+    ami = "ami-007020fd9c84e18c7"
+    instance_type = "t2.micro"
+    key_name = "sql"
+    tags = {
+      Name = "ec1 ${count.index}"
+    }
+    count = 2
+  
+}
