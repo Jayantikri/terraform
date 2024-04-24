581f91f1e64e00b642c7e93d1b579b1c5dda362c
Jayantikri <155369897+Jayantikri@users.noreply.github.com>
2024-04-24 18:16:27 +0530

    Create sg_lb.tf

---
+++ sg_lb.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
+  description = "Allow TLS inbound traffic"
+  
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+        description = "for HTTP"
+        from_port = 80
+        to_port = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_sg"
+  }
+}
